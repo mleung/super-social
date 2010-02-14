534f4bf453f3cd21568cb7be769b75454a3b2826
Michael Leung <blamemike@gmail.com>
2010-02-14 18:04:47 -0500

    Escaping params in append

--- lib/super-social/web.rb
+++ lib/super-social/web.rb
@@ -1,7 +1,7 @@
 module SuperSocial
   module Web
     def append_params(params = {})
-      params.empty? ? "" : "?#{params.collect { |o| o.join("=") }.join("&")}"
+      params.empty? ? "" : "?#{params.collect { |o| CGI.escape(o).join("=") }.join("&")}"
     end
 
     def parse_response(json)
